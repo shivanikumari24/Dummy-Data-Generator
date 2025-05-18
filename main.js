56cd5b833e3c351e5a245a30383563e3b5a22cd6
shivani jha <78164260+shivanikumari24@users.noreply.github.com>
2025-05-18 20:21:18 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,13 +2,11 @@ import mongoose from "mongoose";
 import express from "express";
 import { Employ } from "./models/Employ.js";
 
-let conn = await mongoose.connect("mongodb://localhost:27017/employees");
+let conn = await mongoose.connect("mongodb://localhost:27017/company");
 const app = express();
 const port = 3000;
 
-const titles = ["Developer", "Manager", "Tester", "Designer"];
-const languages = ["python", "react", "javaScript", "php", "Express"];
-const cities = ["Noida", "delhi", "dehradun", "Mohali", "Bettiah"];
+app.set("view engine", "ejs");
 
 function getRandomItem(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
@@ -17,23 +15,32 @@ function getRandomSalary(min = 20000, max = 100000) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-app.get("/", async (req, res) => {
+app.get("/generate", async (req, res) => {
+  //clear the employee
+  await Employ.deleteMany({});
+  //generate random data
+  let titles = ["Developer", "Manager", "Tester", "Designer"];
+  let languages = ["python", "react", "javaScript", "php", "Express"];
+  let cities = ["Noida", "delhi", "dehradun", "Mohali", "Bettiah"];
+
   for (let i = 1; i <= 10; i++) {
     const title = getRandomItem(titles);
 
     const emp = new Employ({
       title: title,
-      salary: getRandomSalary(),
+      salary: getRandomSalary() ,
       language: getRandomItem(languages),
       city: getRandomItem(cities),
-      isManager: title ==="Manager"
+      isManager: title === "Manager",
     });
     await emp.save();
   }
   console.log("✅ 10 random employees created without faker!");
-  res.send("Hello World34!");
 });
 
+app.get("/", (req, res) => {
+  res.render("index", { foo: "FOO" });
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
